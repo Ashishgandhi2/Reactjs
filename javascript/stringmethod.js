0b86f57a1c4c244ea14cda2d152126f658402b26
Ashish <Ashish.gandhi@softobiz.com>
2021-05-25 18:52:37 +0530

    string method

---
+++ javascript/stringmethod.js
@@ -0,0 +1,9 @@
+let str = "This is a javascript program ";
+ console.log(str.indexOf("a"));                                          //indexof
+ console.log(str.lastIndexOf("a"));                                     // last index 
+ console.log(str.indexOf("is", 15));                                    // index start from 15.
+ console.log(str.search("java"));                                       // searching
+ console.log(str.replace("program","tutorial"));                        // replace word
+ let string = "Dog, Cat, Rat";
+console.log(string.slice(2, 6));                                        //slice
+
