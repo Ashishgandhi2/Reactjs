3b37dfb9c096517e76d851baf056e59fd7ee6fca
Ashish <Ashish.gandhi@softobiz.com>
2021-05-25 15:23:30 +0530

    operators and if else

---
+++ javascript/operators.js
@@ -0,0 +1,15 @@
+let a =20;
+let b =100;
+
+if( a==20 && b==30)
+{
+    console.log("True value");
+}
+else if(a==20 && b!=30)
+{
+    console.log("Value B is not matched");
+}
+else
+{
+    console.log("Values are not matched");
+}
