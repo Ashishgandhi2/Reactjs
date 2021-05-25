b8ac6998f98490393588ccaf542fe964bf0af2e8
Ashish <Ashish.gandhi@softobiz.com>
2021-05-25 15:21:25 +0530

    datatypes,strings and variables

---
+++ javascript/datatypes.js
@@ -0,0 +1,13 @@
+const n = "Ashish";                         //const datatypes 
+let string = "Javascript Program";          //string variable
+let a = 20 , b = 30;                        //let datatypes
+let c;
+let d;
+
+console.log(n);
+console.log(string);
+c=a+b;                                      //apply condition 
+console.log(c);                       
+c=a-b;                                      //apply condition
+console.log(c);                     
+console.log(d);                         //undefined data type
