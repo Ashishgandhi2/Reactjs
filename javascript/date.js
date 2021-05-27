a82236db501c810c63519c33ae2455e59d4a1efc
Ashish <Ashish.gandhi@softobiz.com>
2021-05-27 18:41:44 +0530

    date

---
+++ javascript/date.js
@@ -0,0 +1,7 @@
+let d=new Date();
+console.log(Date());
+console.log(d.toDateString());                  //toDateString() method converts a date to a more readable format
+console.log(d.toUTCString());                   //toUTCString() method converts a date to a UTC string
+console.log(d.toISOString());                   //toISOString() method converts a date to a string
+console.log(d.getFullYear());                   // get year
+console.log(d.getMonth());                      // get month
