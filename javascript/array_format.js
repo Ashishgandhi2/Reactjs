7ba2c5e7582e5006ec77b95e90f2e491a119f5b1
Ashish <Ashish.gandhi@softobiz.com>
2021-05-27 11:59:59 +0530

    array format

---
+++ javascript/array_format.js
@@ -0,0 +1,4 @@
+let mobile2=["iphone","vivo"];
+let mobile1=["samsung","oneplus"];
+console.log(mobile=mobile1.concat(mobile2));                //Merging (C
+console.log(mobile1.concat("nokia"));                       //Merging an Array with Values
