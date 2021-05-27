e8f179a933593f52375599ffa36430b1cfda4c21
Ashish <Ashish.gandhi@softobiz.com>
2021-05-27 11:25:52 +0530

    array

---
+++ javascript/array.js
@@ -0,0 +1,10 @@
+let mobile = ["iphone", "oneplus","samsung", "vivo","oppo"];
+console.log(mobile[1]);                         //Access the Elements of an Array
+mobile[4]="nokia";                             // Changing an Array Element
+console.log(mobile);
+console.log(mobile.length);                     //The length Property
+console.log(mobile[mobile.length-1]);           //Accessing the Last Array Element
+console.log(mobile.push("oppo"));               //Adding Array Elements
+console.log(mobile);                            //Access the Full Array
+console.log(typeof(mobile));                    //How to Recognize an Array
+console.log(Array.isArray(mobile));             // Check that is this array or not
