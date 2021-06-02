9d74c59003829c8c04d3d01153655a9372a9150f
Ashish <Ashish.gandhi@softobiz.com>
2021-06-02 10:09:55 +0530

    Regular expresion

---
+++ javascript/Regx.js
@@ -0,0 +1,12 @@
+let str = "My name is John";
+console.log(str.search(/john/i));          // case-insensitive search with a Regular Expression
+let str1 = str.replace(/john/i, "Marry");  // Regular expression to replace
+console.log(str1);
+ 
+let y = /e/.exec("My name is Alex");
+console.log(y);
+
+
+let str2 = "Ashish Gandhi";
+let res = str2.replace("Ashish", "Ashu");       //Replace word
+console.log(res);
