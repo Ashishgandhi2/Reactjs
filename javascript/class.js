84c8d5d4c5d9ccda2b4fd5461fb8de335d731560
Ashish <Ashish.gandhi@softobiz.com>
2021-06-02 10:31:45 +0530

    class

---
+++ javascript/class.js
@@ -0,0 +1,9 @@
+class Car {
+    constructor(name, year) {           //Cosntructor method
+      this.name = name;
+      this.year = year;
+    }
+    
+  }
+ let car1 =new Car("Ford",2014);            // get value
+ console.log(car1);                         // print value
