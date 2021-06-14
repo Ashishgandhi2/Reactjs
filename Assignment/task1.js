269b4a66e7de9953a34ab241ef6c5a962016894c
Ashish <Ashish.gandhi@softobiz.com>
2021-06-14 10:11:16 +0530

    update

--- Assignment/task1.js
+++ Assignment/task1.js
@@ -1,15 +1,27 @@
+// Task 1: replace alais with name
 const a = [
-    {name: 'first_name', alais: 'First Name', type: 'string'},
-    {name: 'age', alais: 'Age', type: 'number'}
-  ];
-  const b = [
-    {first_name: 'John', age: 23},
-    {first_name: 'Tom', age: 25}
-  ];
-let c = b.map(function(obj) {
-   console.log(obj);
-    obj[a[0].alais] = obj['first_name'];      obj[a[1].alais] = obj['age']; 
-    delete obj['first_name']; delete obj['age'];
-    return obj;
-});
+  {name: 'first_name', alais: 'First Name', type: 'string'},
+  {name: 'age', alais: 'Age', type: 'number'}
+];
+const b = [
+  {first_name: 'John', age: 23},
+  {first_name: 'Tom', age: 25}
+];
+function replaceNameToAlais(a, b) {
+  const output = [];
+ 
+  for (let object of b) {
+    let newobject = {};
+    let i = 0;
+    for (key in object) {
+      const newKey = a[i].alais;
+      newobject[newKey] = object[key];
+      i++;
+    }
+    output.push(newobject);
+  }
+  return output;
+}
+ 
+const c = replaceNameToAlais(a, b);
 console.log(c);
